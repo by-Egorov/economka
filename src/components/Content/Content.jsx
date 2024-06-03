b555315ed67f04f2651d4ac87e3c3f2b4ca5a38b
by-Egorov <egorov.dev@gmail.com>
2024-06-03 06:32:20 +0300

    fix

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -19,7 +19,7 @@ const	option = {
 		},
 		series: [
 			{
-				name: 'Radius Mode',
+				name: 'Расходы',
 				type: 'pie',
 				radius: [20, 140],
 				center: ['50%', '50%'],
@@ -36,14 +36,12 @@ const	option = {
 					}
 				},
 				data: [
-					{ value: 40, name: 'rose 1' },
-					{ value: 33, name: 'rose 2' },
-					{ value: 28, name: 'rose 3' },
-					{ value: 22, name: 'rose 4' },
-					{ value: 20, name: 'rose 5' },
-					{ value: 15, name: 'rose 6' },
-					{ value: 12, name: 'rose 7' },
-					{ value: 10, name: 'rose 8' }
+					{ value: 40, name: 'Продукты' },
+					{ value: 33, name: 'Вещи' },
+					{ value: 28, name: 'Машина' },
+					{ value: 22, name: 'Бурундук' },
+					{ value: 20, name: 'Бобряша' },
+					{ value: 15, name: 'Суслик' },
 				]
 			},
 		]
